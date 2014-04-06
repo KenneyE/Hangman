65ffd978093e1a55816dde804e0d74f4cca994ff
Eric Kenney <ekenney5@gmail.com>
2014-04-05 21:09:39 -0700

    Added human class. Working version

--- hangman.rb
+++ hangman.rb
@@ -1,39 +1,48 @@
 class Hangman
 
   MAX_GUESSES = 7
+  attr_reader :guesser, :picker
 
-  def initalize
+  def initialize(guesser, picker)
+    @guesser = guesser
+    @picker = picker
   end
 
   def play
-    @guesser = HumanPlayer.new
-    @picker  = ComputerPlayer.new
-
-
     word_length = @picker.pick_secret_word
     @guesser.receive_secret_length(word_length)
 
     current_state = ["_"] * word_length
+    guessed_letters = []
     guess_count = 0
-    if won?(current_state) && guess_count <= MAX_GUESSES
-
+    until won?(current_state) || guess_count > MAX_GUESSES
+      puts "------------------------------------"
       display_state(current_state)
 
-      guessed_letter = @guesser.guess
-      response = @picker.check_guess(guessed_letter)
+      begin
+        guessed_letter = @guesser.guess
+        puts "Already guessed '#{guessed_letter}'" if guessed_letters.include?(guessed_letter)
+      end while guessed_letters.include?(guessed_letter)
 
+      guessed_letters << guessed_letter
+      response = @picker.check_guess(guessed_letter)
 
       if response.empty?
         guess_count +=1
+        puts "Wrong guess."
       else
         response.each { |ind| current_state[ind] = guessed_letter }
+        puts "Correct guess"
       end
 
       @guesser.handle_guess_response(response, guessed_letter)
-
     end
 
-    puts "The word was guessed!" unless won?(current_state)
+    if won?(current_state)
+    puts "The word was '#{current_state.join}'!"
+    else
+      puts "The word was not guessed!"
+    end
   end
 
   def won?(current_state)
@@ -46,53 +55,81 @@ class Hangman
 
 end
 
-def HumanPlayer
+class HumanPlayer
 
   def pick_secret_word
     puts "Pick a word.  How long is it?"
     gets.chomp.strip.to_i
   end
 
+  def receive_secret_length(word_length)
+    puts "The word is #{word_length} letters long."
+  end
+
+  def guess
+    print "Guess a letter: "
+    gets.chomp.strip.downcase
+  end
+
+  def check_guess(guessed_letter)
+    puts "The other player guessed #{guessed_letter}"
+    puts "Enter the position(s) of the letter (Press 'Enter' if none)"
+    inds = gets.chomp.split(",")
+    inds.map { |ind| ind.to_i - 1 }
+  end
+
+  def handle_guess_response(response, guessed_letter)
+
+  end
 end
 
 class ComputerPlayer
 
-  attr_accessor :word
-  attr_reader :dict
+  attr_accessor :word, :dict
+  attr_reader :guessed_letters
 
   def initialize
-    @dict = intake_dict('./dictionary.txt')
+    @dict = nil
     @word = nil
+    @guessed_letters = []
   end
 
   def pick_secret_word
-    word = dict.sample
-    word.length
+    self.word = intake_dict('./dictionary.txt').sample
+    self.word.length
   end
 
   def intake_dict(file)
-    dictionary = File.readlines(file).chomp
+    self.dict = File.readlines(file).map { |word| word.chomp }
   end
 
   def receive_secret_length(word_length)
-    dict.select!{ | word | word.length == word_length }
+    self.dict = intake_dict('./dictionary.txt')
+    self.dict.select!{ | dict_word | dict_word.length == word_length }
   end
 
   def guess
-    dict.sample.split(//).sample
+    begin
+      letter = self.dict.sample.split(//).sample
+    end until !guessed_letters.include?(letter) && letter =~ /[a-z]/
+    guessed_letters << letter
+    letter
   end
 
   def check_guess(guessed_letter)
     correct_inds = []
 
-    word.each_with_index { |letter, ind| correct_inds << ind if word.include?(letter) }
+    self.word.split(//).each_with_index do |letter, ind|
+      correct_inds << ind if letter == guessed_letter
+    end
 
     correct_inds
   end
 
-  def handle_guess_response(response, guessed_letter)
+  def handle_guess_response(response, letter)
+
     response.each do |ind|
-      dict.select! { |word| word =~ /\A.{ind-1}[letter]/ }
+      self.dict.select! { |word| word =~ /\A.{#{ind}}[#{letter}]/ }
     end
   end
 end
